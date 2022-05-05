e7fc196369e95ef52df8af3b3c03313e4aafa8db
tdv14011984@gmail.com <Parol2022!-forGIT>
2022-05-05 12:51:48 +0300

    add TestBase

--- src/test/java/denis/timushev/tests/TestBase.java
+++ src/test/java/denis/timushev/tests/TestBase.java
@@ -1,7 +1,9 @@
 package denis.timushev.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import denis.timushev.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -11,8 +13,10 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1980x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
